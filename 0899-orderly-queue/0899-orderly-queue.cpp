4c4a1f8de000730b0cc55de7db494aa4747a8fc9
Unnati1203 <unnatigupta212@gmail.com>
2026-09-02 12:32:44 +0530

    Time: 3 ms (62.22%), Space: 13.4 MB (58.7%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k>1){
+          sort(s.begin(),s.end());
+          return s;
+        }
+        string ans=s;
+        if(k==1){
+           
+           
+           for(int i=0;i<s.size();i++){
+            string temp= s.substr(i)+s.substr(0,i);
+            if(temp<ans){
+                ans= temp;
+            }
+           } 
+        }
+        return ans;
+        
+    }
+};
